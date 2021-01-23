aadf38e8dd6a1f8675fc9e69d5330530bf11e732
Fabio Rodrigues Guerra <fabioguerrra@hotmail.com>
2021-01-23 16:59:40 -0300

    Update wifi_passwords.py

--- wifi_passwords.py
+++ wifi_passwords.py
@@ -1,35 +1,19 @@
-#!/usr/bin/env python3
-
+import os
 import subprocess
-import re
-
-print("\n[+] Search for wifi passwords (Linux OS)")
-print("[+] SSID: PASS\n")
 
 path = "/etc/NetworkManager/system-connections"
+os.chdir(path)
+wifi_list = os.listdir()
 
-wifi = subprocess.getoutput("cd " + path + " && ls")
-wifi_ssid = re.compile("[a-z0-9A-Z -_]+.nmconnection") # search for ssid RE
+wifi_pass = {}
 
-wifi_dump = wifi_ssid.findall(wifi)
-wifi_pass = {} 
-
-for ssid in wifi_dump:
-    
-    str_ssid = ssid.replace(" ", "\ ")
-    full_path = path + "/" + str_ssid
-   
-    CMD  = "cat " + full_path +  "|" + "grep " + "psk=" 
-    
-    # cat /etc/NetworkManager/system-connections/{ssid}.nmconnection | grep psk=
-    
-    password = subprocess.getoutput(CMD)
-
-    str_ssid = str_ssid.replace("\ ", " ")
-    str_ssid = str_ssid.replace(".nmconnection", "")
-    wifi_pass[str_ssid] = password.replace("psk=", "")
-    print("{0}: {1}".format(str_ssid, wifi_pass[str_ssid]))
+for wifi in wifi_list:
+  
+    wifi_path = wifi.replace(" ", "\ ")
+    ssid = subprocess.getoutput("cat " + wifi_path + " | " + "grep ssid=")
+    psk = subprocess.getoutput("cat " + wifi_path + " | " + "grep psk=")
+    wifi_pass[ssid.replace("ssid=","")] = psk.replace("psk=","")
 
 
-print()
-
+for chaves in wifi_pass.keys():
+    print("{0} : {1}".format(chaves, wifi_pass[chaves]))
